a5a228060676ea2026e6435775cc365bb5eec964
Matthew Kaufer <mjkaufer@gmail.com>
2014-11-11 15:26:35 -0500

    Imgur API interfacing

--- app/src/main/java/org/kaufer/matthew/sniksnak/Main.java
+++ app/src/main/java/org/kaufer/matthew/sniksnak/Main.java
@@ -2,21 +2,48 @@ package org.kaufer.matthew.sniksnak;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.hardware.Camera;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Base64;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.firebase.client.Firebase;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.apache.http.Header;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStreamWriter;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+import java.util.List;
 
 
 public class Main extends Activity {
     Firebase sniks;
     private Location lastLocation;
+    private Camera camera;
+    private ImageView imgView;
+    AsyncHttpClient client = new AsyncHttpClient();
+    TextView t;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +51,47 @@ public class Main extends Activity {
         Firebase.setAndroidContext(this);
         sniks = new Firebase("https://sniksnak.firebaseio.com/").child("sniks");
         Button b = (Button)findViewById(R.id.button);
+        Button c = (Button)findViewById(R.id.button2);
+        t = (TextView)findViewById(R.id.textView);
+        camera = Camera.open();
+        c.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                camera.startPreview();
+                camera.takePicture(null, null, new Camera.PictureCallback() {
+                    @Override
+                    public void onPictureTaken(byte[] bytes, Camera camera) {
+                        try {
+                            Bitmap bMap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);//turns taken picture into bitmap
+                            imgView.setImageBitmap(bMap);//sets the image of imgView to the freshly taken image
+                            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                            bMap.compress(Bitmap.CompressFormat.PNG, 100, baos); // Not sure whether this should be jpeg or png, try both and see which works best
+                            String url = "https://api.imgur.com/3/upload.json";
+                            String data = URLEncoder.encode("image", "UTF-8") + "=" + URLEncoder.encode(Base64.encode(baos.toByteArray(), Base64.DEFAULT).toString(), "UTF-8");
+                            data += "&" + URLEncoder.encode("key", "UTF-8") + "=" + URLEncoder.encode("da9f0c5a31cf2a6", "UTF-8");
+                            String actualUrl = url + "?" + data;
+                            client.post(actualUrl, null, new JsonHttpResponseHandler(){
+                                public void onSuccess(int status, Header[] headers, String response){
+                                    System.out.println(response);
+                                    t.setText(response);
+                                }
+
+                                public void onFailure(int status, Header[] headers, String response){
+                                    System.out.println(response);
+                                    t.setText(response);
+                                }
+                            });
+
+
+                        } catch(Exception e){
+                            System.out.println("You messed up");
+                        }
+                    }
+                });
+            }
+        });
+
+
         b.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
