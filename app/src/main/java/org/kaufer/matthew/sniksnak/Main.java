705128f9f090e92b9e0bcd8e83c38c5bde01852b
Matthew Kaufer <mjkaufer@gmail.com>
2014-11-11 14:29:37 -0500

    Can push random data!

--- app/src/main/java/org/kaufer/matthew/sniksnak/Main.java
+++ app/src/main/java/org/kaufer/matthew/sniksnak/Main.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import com.firebase.client.Firebase;
 
@@ -16,6 +18,13 @@ public class Main extends Activity {
         setContentView(R.layout.activity_main);
         Firebase.setAndroidContext(this);
         sniks = new Firebase("https://sniksnak.firebaseio.com/").child("sniks");
+        Button b = (Button)findViewById(R.id.button);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sniks.push().setValue(new Snik(new double[]{Math.random() * 100, Math.random() * 100}, "http://google.com","Hello World").toHashMap());
+            }
+        });
     }
 
 
