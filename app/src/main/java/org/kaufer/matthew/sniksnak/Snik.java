4ab998241c7add564b7f9ef8fe55c313a673259b
Matthew Kaufer <mjkaufer@gmail.com>
2014-11-11 14:19:41 -0500

    Finished Snik class

--- app/src/main/java/org/kaufer/matthew/sniksnak/Snik.java
+++ app/src/main/java/org/kaufer/matthew/sniksnak/Snik.java
@@ -1,18 +1,22 @@
 package org.kaufer.matthew.sniksnak;
 
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * Created by 2016mkaufer on 11/10/2014.
  */
 public class Snik {
-    double[] coordinates;//TODO - MAKE COORDINATES A LIST SO WE CAN PASS IT IN THE HASHMAP
+    List<Double> coordinates;//TODO - MAKE COORDINATES A LIST SO WE CAN PASS IT IN THE HASHMAP
     String url;
     String text;
     int score;
 
     public Snik(double[] coord,String u, String t){
-        coordinates = coord;
+        coordinates = new LinkedList<Double>();//we make it a list because firebase likes lists more than arrays, apparently
+        coordinates.add(0,coord[0]);
+        coordinates.add(1,coord[0]);
         url = u;
         text = t;
         score = 0;
@@ -36,10 +40,10 @@ public class Snik {
 
     public HashMap<String, Object> toHashMap(){
         HashMap<String, Object> s = new HashMap<String, Object>();
-
-
-
-
-
+        s.put("url",url);
+        s.put("text",text);
+        s.put("score",score);
+        s.put("coordinates",coordinates);
+        return s;
     }
 }
