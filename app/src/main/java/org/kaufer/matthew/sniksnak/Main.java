270cb2c6562a7482ba121b2e132788ca05213dfa
Matthew Kaufer <mjkaufer@gmail.com>
2014-11-13 15:46:20 -0500

    No pictures for now - text should work

--- app/src/main/java/org/kaufer/matthew/sniksnak/Main.java
+++ app/src/main/java/org/kaufer/matthew/sniksnak/Main.java
@@ -14,6 +14,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -43,6 +44,7 @@ public class Main extends Activity {
     private ImageView imgView;
     AsyncHttpClient client = new AsyncHttpClient();
     TextView t;
+    EditText et;
 
     private boolean safeToTakePicture = false;
 
@@ -56,6 +58,7 @@ public class Main extends Activity {
         Button b = (Button)findViewById(R.id.button);
         Button c = (Button)findViewById(R.id.button2);
         t = (TextView)findViewById(R.id.textView);
+        et = (EditText)findViewById(R.id.editText);
         camera = Camera.open(0);
         Camera.CameraInfo ci = new android.hardware.Camera.CameraInfo();
         Camera.getCameraInfo(0, ci);
@@ -64,40 +67,43 @@ public class Main extends Activity {
         c.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                camera.stopPreview();
-                camera.startPreview();
-                camera.takePicture(null, null, new Camera.PictureCallback() {
-                    @Override
-                    public void onPictureTaken(byte[] bytes, Camera camera) {
-                        try {
-                            Bitmap bMap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);//turns taken picture into bitmap
-                            imgView.setImageBitmap(bMap);//sets the image of imgView to the freshly taken image
-                            ByteArrayOutputStream baos = new ByteArrayOutputStream();
-                            bMap.compress(Bitmap.CompressFormat.PNG, 100, baos); // Not sure whether this should be jpeg or png, try both and see which works best
-                            String url = "https://api.imgur.com/3/upload.json";
-                            String data = URLEncoder.encode("image", "UTF-8") + "=" + URLEncoder.encode(Base64.encode(baos.toByteArray(), Base64.DEFAULT).toString(), "UTF-8");
-                            data += "&" + URLEncoder.encode("key", "UTF-8") + "=" + URLEncoder.encode("da9f0c5a31cf2a6", "UTF-8");
-                            String actualUrl = url + "?" + data;
-                            client.post(actualUrl, null, new JsonHttpResponseHandler(){
-                                public void onSuccess(int status, Header[] headers, String response){
-                                    System.out.println(response);
-                                    t.setText(response);
-                                }
+//                camera.stopPreview();
+//                camera.startPreview();
+//                camera.takePicture(null, null, new Camera.PictureCallback() {
+//                    @Override
+//                    public void onPictureTaken(byte[] bytes, Camera camera) {
+//                        try {
+//                            Bitmap bMap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);//turns taken picture into bitmap
+//                            imgView.setImageBitmap(bMap);//sets the image of imgView to the freshly taken image
+//                            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+//                            bMap.compress(Bitmap.CompressFormat.PNG, 100, baos); // Not sure whether this should be jpeg or png, try both and see which works best
+//                            String url = "https://api.imgur.com/3/upload.json";
+//                            String data = URLEncoder.encode("image", "UTF-8") + "=" + URLEncoder.encode(Base64.encode(baos.toByteArray(), Base64.DEFAULT).toString(), "UTF-8");
+//                            data += "&" + URLEncoder.encode("key", "UTF-8") + "=" + URLEncoder.encode("da9f0c5a31cf2a6", "UTF-8");
+//                            String actualUrl = url + "?" + data;
+//                            client.post(actualUrl, null, new JsonHttpResponseHandler(){
+//                                public void onSuccess(int status, Header[] headers, String response){
+//                                    System.out.println(response);
+//                                    t.setText(response);
+//                                }
+//
+//                                public void onFailure(int status, Header[] headers, String response){
+//                                    System.out.println(response);
+//                                    t.setText(response);
+//                                }
+//                            });
+//
+//
+//                        } catch(Exception e){
+//                            System.out.println("You messed up");
+//                        }
+//                    }
+//                });
+                //we're not going to do images because it's not fun...
+//                camera.stopPreview();
 
-                                public void onFailure(int status, Header[] headers, String response){
-                                    System.out.println(response);
-                                    t.setText(response);
-                                }
-                            });
+                sniks.push().setValue(new Snik(new double[]{Math.random() * 100, Math.random() * 100}, "http://google.com",et.getText().toString()).toHashMap());
 
-
-                        } catch(Exception e){
-                            System.out.println("You messed up");
-                        }
-                    }
-                });
-
-                camera.stopPreview();
 
             }
         });
